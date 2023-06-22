f76d37a7cab3055d1c35b04a3a1717a4e9979a64
Sanjin <102841251+duckduckgrayduck@users.noreply.github.com>
2023-06-22 14:55:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,12 @@ class PageDeleter(AddOn):
             file_handle = fitz.open(input_file)
             for page in range(1,document.pages+1):
                 print(f"{page}")
+                to_include =[] 
                 if document.get_page_text(page).isspace() or document.get_page_text(page)=="":
-                    file_handle.delete_page(page-1)
-                    print(f"Deleted page{page}")
+                   pass
+                else: 
+                    to_include.append(page-1)
+            file_handle.select(to_include)
             file_handle.save(output_file)
         self.client.documents.upload_directory("./out/")
 
